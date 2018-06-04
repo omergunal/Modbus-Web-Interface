0b9aba8278dc23ee7e527d8e8ef4664d72987104
Ömer Günal <o.gunal977@gmail.com>
2018-06-04 18:42:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 
 class ReusableForm(Form):
-    ip = TextField('IP:', validators=[validators.required()])
-    port = TextField('Port:', validators=[validators.required(), validators.Length(min=1, max=65536)])
-    adres = TextField('Baslangic Adresi:', validators=[validators.required(), validators.Length(min=0, max=65536)])
-    deger = TextField('IP:', validators=[validators.required()])
-    unitId = TextField('unitId:', validators=[validators.required()])
+    ip = TextField('IP:')
+    port = TextField('Port:')
+    adres = TextField('Baslangic Adresi:')
+    deger = TextField('IP:')
+    unitId = TextField('unitId:')
 
 @app.route("/")
 def index():
@@ -30,29 +30,25 @@ def read_holding_regs():
     if request.method == 'POST':
         ip=request.form['ip']
         port=request.form['port']
-        adres=request.form['adres']
-        deger=request.form['deger']
+        address=request.form['address']
+        value=request.form['value']
         unitId=request.form['unitId']
-        print ip, " ", port, " ", adres
+        print ip, " ", port, " ", address
 
         host = ip
         port = int(port)
         client = ModbusTcpClient(host, port)
         client.connect()
 
-        rr = client.read_holding_registers(int(adres),int(deger),unit= int(unitId))
+        rr = client.read_holding_registers(int(address),int(value),unit= int(unitId))
         assert(rr.function_code < 0x80)     # test that we are not an error
         print (rr)
-        print (rr.registers)
-
-
+        print (str(rr.registers))
 
         if form.validate():
-            # Save the comment here.
-            flash(rr.registers)
-            #flash('Thanks for registration ' + ip)
+            flash(str(rr.registers))
         else:
-            flash('Error: All the form fields are required. ')
+            flash("Error")
 
     return render_template('read-hold-regs.html', form=form)
 
@@ -79,50 +75,42 @@ def write_regs():
             #print (rr)
             #print (rr.registers)
 
-
-
             if form.validate():
                 # Save the comment here.
-                flash("Success")
+                flash("Success!")
                 #flash('Thanks for registration ' + ip)
             else:
-                flash('Error: All the form fields are required. ')
+                flash('Error')
 
         return render_template('write-register.html', form=form)
 
 @app.route("/read-coils", methods=['GET', 'POST'])
-def coils():
+def read_coils():
     form = ReusableForm(request.form)
 
     print form.errors
     if request.method == 'POST':
         ip=request.form['ip']
         port=request.form['port']
-        adres=request.form['adres']
-        deger=request.form['deger']
+        address=request.form['address']
+        value=request.form['value']
         unitId=request.form['unitId']
-        print ip, " ", port, " ", adres
+        print ip, " ", port, " ", address
 
         host = ip
         port = int(port)
         client = ModbusTcpClient(host, port)
         client.connect()
 
-        rr = client.read_coils(int(adres),int(deger),unit= int(unitId))
+        rr = client.read_coils(int(address),int(value),unit= int(unitId))
         assert(rr.function_code < 0x80)     # test that we are not an error
         print (rr)
         print (rr.bits)
 
-
-
         if form.validate():
-            # Save the comment here.
-
-                flash(str(rr.bits))
-            #flash((rr.bits))
-            #flash('Thanks for registration ' + ip)
+            flash(str(rr.bits))
         else:
-            flash('Error: All the form fields are required. ')
+            flash('Error')
 
     return render_template('read-coils.html', form=form)
 
@@ -145,19 +133,14 @@ def write_coils():
         client.connect()
 
         rr = client.write_coils(int(address),int(value),unit= int(unitId))
-        #assert(rr.function_code < 0x80)     # test that we are not an error
-
-
-
+        assert(rr.function_code < 0x80)     # test that we are not an error
 
         if form.validate():
-            # Save the comment here.
             flash("Success")
         else:
-            flash('Error: All the form fields are required. ')
+            flash('Error')
 
     return render_template('write-coils.html', form=form)
-
 
 
 if __name__ == "__main__":
